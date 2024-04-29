9c0134ac1d37410bf3c658eed14e673e74d26de3
hugocharels <hugocharels@hotmail.com>
2024-04-29 22:33:55 +0200

    :tada: init commit

---
+++ src/main/java/org/hikuro/hikucraft/SemiRP.java
@@ -0,0 +1,17 @@
+package org.hikuro.hikucraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SemiRP extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("SemiRP has been enabled.");
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("SemiRP has been disabled.");
+	}
+
+}
